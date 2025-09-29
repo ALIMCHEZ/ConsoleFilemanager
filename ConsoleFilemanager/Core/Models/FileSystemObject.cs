bcb0e3d82d88ba099fd2af4a8884efa253101c05
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-09-29 15:51:30 +0300

    Added class for files/directories

---
+++ ConsoleFilemanager/Core/Models/FileSystemObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFilemanager.Core.Models
+{
+    internal class FileSystemObject
+    {
+        private string _path;
+        public required string Path    
+        {
+            get => _path;
+            init
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+                else
+                {
+                    _path = value;
+                }
+            }
+        }
+
+        public readonly FileSystemObjectType Type;
+
+        public FileSystemObject(string fileSystemObjectPath)
+        {
+            Path = fileSystemObjectPath;
+        }
+    }
+}
