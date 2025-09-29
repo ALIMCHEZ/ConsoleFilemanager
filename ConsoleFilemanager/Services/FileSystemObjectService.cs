b434741a0741d7df7a281d8626b1db56a6cbb304
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-09-29 18:42:44 +0300

    Class name changed

---
+++ ConsoleFilemanager/Services/FileSystemObjectService.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleFilemanager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleFilemanager.Services
+{
+    internal class FileSystemObjectService : IFileSystemObjectService
+    {
+        public async Task DeleteAsync(string path)
+        {
+            File.Delete(path);
+        }
+
+        public async Task<List<FileSystemObject>> GetFilesAsync(string path)
+        {
+            return new DirectoryInfo(path).GetFileSystemInfos().Select(f => new FileSystemObject() { FileInfo = f }).ToList();
+        }
+    }
+}
