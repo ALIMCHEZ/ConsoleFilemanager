04f5a279a13a1f0364ead4ba8734dd9e0764cfca
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-09-29 05:09:44 +0300

    Added IFileService interfance and FileService classes to get files from dir

---
+++ ConsoleFilemanager/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFilemanager.Services
+{
+    internal interface IFileService
+    {
+        Task<List<string>> GetFilesAsync(string path);
+        Task DeleteAsync(string path);
+    }
+}
