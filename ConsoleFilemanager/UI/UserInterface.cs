ccca358ab4f62e21707f2425ccbb3517c50ce181
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-09-30 18:22:18 +0300

    Added simle UI Class

---
+++ ConsoleFilemanager/UI/UserInterface.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleFilemanager.Core.Models;
+using ConsoleFilemanager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFilemanager.UI
+{
+    internal class UserInterface
+    {
+        private FileSystemObjectService _objectService { get; set; }
+        private List<FileSystemObject> fileSystemObjects { get; set; }
+
+        public UserInterface(string startFolder)
+        {
+            _objectService = new FileSystemObjectService();
+            fileSystemObjects = _objectService.GetFilesAsync(startFolder).Result;
+            BuilUi();
+        }
+
+        private void BuilUi()
+        {
+            foreach (var file in fileSystemObjects)
+            {
+                Console.WriteLine(file.FileInfo.Name);
+            }
+        }
+    }
+}
