1f7da46337e104b6764ecc7ce21ffe25964de266
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-10-02 23:31:43 +0300

    Fix: stringBuilder = new StringBuilder=>stringBuilder.Clear(), updating selectIndex when go to anogher folder

--- ConsoleFilemanager/UI/UserInterface.cs
+++ ConsoleFilemanager/UI/UserInterface.cs
@@ -11,13 +11,15 @@ namespace ConsoleFilemanager.UI
 {
     internal class UserInterface
     {
-        private FileSystemObjectService _objectService { get; set; }
+        private FileSystemObjectService _objectService = new FileSystemObjectService();
         private List<FileSystemObject> fileSystemObjects { get; set; }
 
-        private StringBuilder _strBuilder;
+        private StringBuilder _strBuilder = new StringBuilder();
         private int selectIndex = 0;
         
         private string _startFolder;
+
+        private bool rebuild = true;
         public string StartFolder
         {
             get=>_startFolder;
@@ -31,21 +33,28 @@ namespace ConsoleFilemanager.UI
 
         private async Task Init(string startFolder)
         {
+            selectIndex = 0;
             StartFolder = startFolder;
-            _objectService = new FileSystemObjectService();
             fileSystemObjects = await _objectService.GetFilesAsync(_startFolder);
 
         }
 
         public async Task RunAsync(string startFolder)
         {
+
+           
             await Init(startFolder);
 
             await Task.Delay(50);
 
             while (true)
             {
-                BuilUi();
+                if (rebuild)
+                {
+                    BuilUi();
+                    rebuild = false;
+                }
+
 
                 if (Console.KeyAvailable)
                 {
@@ -59,15 +68,15 @@ namespace ConsoleFilemanager.UI
 
         private void BuilUi()
         {
-            Console.Clear();
             BuildCurrentfolderWindow();
             BuildFilesWindow();
         }
 
         private void BuildFilesWindow()
         {
+
             var max = fileSystemObjects.Max(f => f.FileInfo.Name.Length);
-            _strBuilder = new StringBuilder();
+            _strBuilder.Clear();
             _strBuilder.Append("+");
             for (int i = 0; i < max; i++)
             {
@@ -87,8 +96,9 @@ namespace ConsoleFilemanager.UI
 
         private void BuildCurrentfolderWindow()
         {
+            Console.Clear();
             var max = fileSystemObjects.Max(fs => fs.FileInfo.Name.Length);
-            _strBuilder = new StringBuilder();
+            _strBuilder.Clear();
             _strBuilder.Append("+");
             for (int i = 0; i < max; i++)
             {
@@ -105,15 +115,24 @@ namespace ConsoleFilemanager.UI
             {
                 case ConsoleKey.UpArrow:
                     if (selectIndex > 0) selectIndex--;
+                    rebuild = true;
                     break;
 
                 case ConsoleKey.DownArrow:
                     if (fileSystemObjects.Count  > selectIndex + 1) selectIndex++;
+                    rebuild = true;
                     break;
                 case ConsoleKey.Enter:
                     if (fileSystemObjects[selectIndex].FileInfo as DirectoryInfo != null)
                         await Init(fileSystemObjects[selectIndex].FileInfo.FullName);
-                            break;
+                    rebuild = true;
+                    break;
+                case ConsoleKey.Backspace:
+                    var dir = new DirectoryInfo(_startFolder);
+                    if (dir?.Parent!=null) 
+                        await Init(dir.Parent.FullName);
+                    rebuild = true;
+                    break;
             }
         }
     }
